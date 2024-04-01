6c34a7cc34f292c98f902e06ce46e494700a3330
rocio <rramirez@birt.eus>
2024-04-01 23:56:32 +0200

    segundo commit sec4cap10

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+//clase
 export class AppComponent {
-  title = 'bases';
+
+  public title: string = 'Hola Mundo';
+  public counter:number = 10;
+
+// metodos de clase
+  increaseBy(value:number):void{
+    this.counter += value;
+  }
+
+  decreaseBy(value:number):void{
+    this.counter -= value;
+  }
+
+  reset():void{
+    this.counter=10;
+  }
+
 }
---
+++ 02-bases/src/app/counter/counter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-counter',
+    templateUrl: '../counter/counter.component.html',
+    styleUrl: '../counter/counter.component.css'
+})
+
+export class CounterComponent {
+
+}
